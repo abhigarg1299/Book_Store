f1697bda0d85bd7d89a99c86d23eaa260c33bf9d
Abhi Garg <abhigarg@Abhis-MacBook-Air.local>
2024-08-19 12:44:19 +0530

    Update-Book added

--- backend/controllers/bookController.js
+++ backend/controllers/bookController.js
@@ -7,11 +7,11 @@ export const addBook = async (req, res) => {
 
     const User = await user.findById(id);
     console.log(User);
-    // if (User.role !== "admin") {
-    //   return res.status(400).json({
-    //     message: "You do not have accesss to perform admin work",
-    //   });
-    // }
+    if (User.role !== "admin") {
+      return res.status(400).json({
+        message: "You do not have accesss to perform admin work",
+      });
+    }
     const book = new books({
       url: req.body.url,
       title: req.body.title,
@@ -24,6 +24,30 @@ export const addBook = async (req, res) => {
     await book.save();
     return res.status(200).json({
       message: "Book Added Successfully",
+      book,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      message: "Internal Server Error",
+    });
+  }
+};
+
+export const updateBook = async (req, res) => {
+  try {
+    const { bookid } = req.headers;
+    console.log(bookid);
+    const book = await books.findByIdAndUpdate(bookid, {
+      url: req.body.url,
+      title: req.body.title,
+      author: req.body.author,
+      price: req.body.price,
+      desc: req.body.desc,
+      language: req.body.language,
+    });
+    return res.status(200).json({
+      message: "Book Updated SuccessFully",
       book,
     });
   } catch (error) {
--- backend/routes/book.js
+++ backend/routes/book.js
@@ -1,7 +1,8 @@
 import express from "express";
 import isAuthenticated from "../middleware/authetication.js";
-import { addBook } from "../controllers/bookController.js";
+import { addBook, updateBook } from "../controllers/bookController.js";
 const router = express.Router();
 
 router.route("/add-book").post(isAuthenticated, addBook);
+router.route("/update-book").put(isAuthenticated, updateBook);
 export default router;
