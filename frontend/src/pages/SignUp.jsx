6deaec53c88eecf098c3bb233c5c953e255fa800
Abhi Garg <94074111+abhigarg1299@users.noreply.github.com>
2024-08-29 21:25:17 +0530

    Update SignUp.jsx

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -25,7 +25,7 @@ function SignUp() {
         alert("All fields are required");
       } else {
         const response = await axios.post(
-          "http://127.0.0.1:1000/api/v1/user/sign-up",
+          "https://book-store-backend-zkn3.onrender.com/api/v1/user/sign-up",
           values
         );
         alert(response.data.message);
