340d764cff5b19017feff2120a2151a1fd0b7902
Abhi Garg <94074111+abhigarg1299@users.noreply.github.com>
2024-08-29 21:23:14 +0530

    Update AllBooks.jsx

--- frontend/src/pages/AllBooks.jsx
+++ frontend/src/pages/AllBooks.jsx
@@ -8,7 +8,7 @@ function AllBooks() {
   useEffect(() => {
     const fetch = async () => {
       const response = await axios.get(
-        "http://127.0.0.1:1000/api/v1/book/get-all-books"
+        "https://book-store-backend-zkn3.onrender.com/api/v1/book/get-all-books"
       );
       setData(response.data.data);
     };
