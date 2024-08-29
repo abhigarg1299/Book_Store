5d4bad7eee893eba0304bf0f948fdfe2225236f6
Abhi Garg <abhigarg@Abhis-MacBook-Air.local>
2024-08-29 21:10:45 +0530

    bcypt

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,5 @@
 import { user } from "../models/user.js";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 export const signUp = async (req, res) => {
   try {
